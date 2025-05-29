e0a75c91b0de0c879cef12e4ac206af51d12de97
TomSumner28 <tom@therewardcollection.com>
2025-05-29 20:14:08 +0100

    t

--- main.jsx
+++ main.jsx
@@ -1,32 +1,8 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function Dashboard() {
-  return <h2>Dashboard</h2>;
-}
-function Team() {
-  return <h2>Team</h2>;
-}
-function KnowledgeBase() {
-  return <h2>Knowledge Base</h2>;
-}
-function QuickResponse() {
-  return <h2>Quick Response</h2>;
-}
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/knowledge-base" element={<KnowledgeBase />} />
-        <Route path="/quick-response" element={<QuickResponse />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>TRC SalesGPT Interface</h1>
+  </React.StrictMode>
+);
