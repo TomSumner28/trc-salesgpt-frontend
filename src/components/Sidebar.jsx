873b7a5e54d0d48e0f07b19e6916987e69ab5442
TomSumner28 <tom@therewardcollection.com>
2025-05-30 15:38:58 +0100

    s

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sidebar = () => (
+  <aside className="w-64 bg-gray-800 p-4">
+    <h1 className="text-2xl font-bold mb-8">TRC SalesGPT</h1>
+    <ul className="space-y-4">
+      <li>Dashboard</li>
+      <li>Team</li>
+      <li>Knowledge Base</li>
+      <li>Quick Response</li>
+    </ul>
+  </aside>
+);
+
+export default Sidebar;
