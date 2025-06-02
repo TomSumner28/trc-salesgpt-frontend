ba4fc7e74688c0a8c9c7d287891e013cc43d1edd
TomSumner28 <tom@therewardcollection.com>
2025-06-02 13:18:38 +0100

    ss

--- src/MicrosoftLoginButton.jsx
+++ src/MicrosoftLoginButton.jsx
@@ -1,20 +1,22 @@
 
-import React from 'react';
+import React from "react";
 
 const MicrosoftLoginButton = () => {
-  const clientId = import.meta.env.VITE_MICROSOFT_CLIENT_ID;
-  const tenant = import.meta.env.VITE_MICROSOFT_TENANT_ID;
-  const redirectUri = import.meta.env.VITE_MICROSOFT_REDIRECT_URI;
+  const clientId = "ec78220d-e785-45bb-84ed-0e89a6c33e81";
+  const tenant = "common";  // or use your specific tenant ID if needed
+  const redirectUri = "https://trc-salesgpt-backend.onrender.com/callback";
 
-  const authUrl = `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&response_mode=query&scope=offline_access https://graph.microsoft.com/Mail.ReadWrite https://graph.microsoft.com/Mail.Send https://graph.microsoft.com/User.Read`;
+  const authUrl = \`https://login.microsoftonline.com/\${tenant}/oauth2/v2.0/authorize?client_id=\${clientId}&response_type=code&redirect_uri=\${redirectUri}&response_mode=query&scope=offline_access%20https://graph.microsoft.com/Mail.ReadWrite%20https://graph.microsoft.com/Mail.Send%20https://graph.microsoft.com/User.Read\`;
 
   return (
-    <button
-      onClick={() => window.location.href = authUrl}
-      style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}
-    >
-      Sign in with Microsoft
-    </button>
+    <div className="flex justify-center mt-12">
+      <a
+        href={authUrl}
+        className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded"
+      >
+        Sign in with Microsoft
+      </a>
+    </div>
   );
 };
 
