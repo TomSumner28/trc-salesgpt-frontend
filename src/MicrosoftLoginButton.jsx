3e2b95bd823dbf2ee0e0585a11911d621b3f6063
TomSumner28 <tom@therewardcollection.com>
2025-06-02 13:23:20 +0100

    jj

--- src/MicrosoftLoginButton.jsx
+++ src/MicrosoftLoginButton.jsx
@@ -1,18 +1,17 @@
-
 import React from "react";
 
 const MicrosoftLoginButton = () => {
   const clientId = "ec78220d-e785-45bb-84ed-0e89a6c33e81";
-  const tenant = "common";  // or use your specific tenant ID if needed
+  const tenant = "758fd55f-1ab3-4b33-b131-bcd948281ff0";
   const redirectUri = "https://trc-salesgpt-backend.onrender.com/callback";
 
-  const authUrl = \`https://login.microsoftonline.com/\${tenant}/oauth2/v2.0/authorize?client_id=\${clientId}&response_type=code&redirect_uri=\${redirectUri}&response_mode=query&scope=offline_access%20https://graph.microsoft.com/Mail.ReadWrite%20https://graph.microsoft.com/Mail.Send%20https://graph.microsoft.com/User.Read\`;
+  const authUrl = `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&response_mode=query&scope=offline_access%20https://graph.microsoft.com/Mail.ReadWrite%20https://graph.microsoft.com/Mail.Send%20https://graph.microsoft.com/User.Read`;
 
   return (
-    <div className="flex justify-center mt-12">
+    <div className="flex justify-center items-center h-screen bg-gray-100">
       <a
         href={authUrl}
-        className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded"
+        className="px-6 py-3 text-white bg-blue-600 rounded hover:bg-blue-700 transition"
       >
         Sign in with Microsoft
       </a>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MicrosoftLoginButton from "./MicrosoftLoginButton";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import MicrosoftLoginButton from './MicrosoftLoginButton';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MicrosoftLoginButton />
   </React.StrictMode>
