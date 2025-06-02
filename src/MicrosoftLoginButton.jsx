2f306c2e1bc2c0432f5df48aa83b3b74be3af4f8
TomSumner28 <tom@therewardcollection.com>
2025-06-02 12:56:40 +0100

    ss

--- src/MicrosoftLoginButton.jsx
+++ src/MicrosoftLoginButton.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+
+import React from 'react';
 
 const MicrosoftLoginButton = () => {
   const clientId = import.meta.env.VITE_MICROSOFT_CLIENT_ID;
   const tenant = import.meta.env.VITE_MICROSOFT_TENANT_ID;
   const redirectUri = import.meta.env.VITE_MICROSOFT_REDIRECT_URI;
 
-  const authUrl = `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&response_mode=query&scope=offline_access%20https://graph.microsoft.com/Mail.ReadWrite%20https://graph.microsoft.com/Mail.Send%20https://graph.microsoft.com/User.Read`;
+  const authUrl = `https://login.microsoftonline.com/${tenant}/oauth2/v2.0/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&response_mode=query&scope=offline_access https://graph.microsoft.com/Mail.ReadWrite https://graph.microsoft.com/Mail.Send https://graph.microsoft.com/User.Read`;
 
   return (
-    <a href={authUrl}>
-      <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Sign in with Microsoft
-      </button>
-    </a>
+    <button
+      onClick={() => window.location.href = authUrl}
+      style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}
+    >
+      Sign in with Microsoft
+    </button>
   );
 };
 
