b8732f5ee57a9f21e77dee4325a1657834031bdb
TomSumner28 <tom@therewardcollection.com>
2025-06-02 13:44:16 +0100

    a

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MicrosoftLoginButton from "./MicrosoftLoginButton";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MicrosoftLoginButton />
-  </React.StrictMode>
-);
